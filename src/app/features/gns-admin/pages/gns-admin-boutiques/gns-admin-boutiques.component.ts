4b9d43a50d882aa85900efd84f914cf02ac60890
J-Cicero <prudencioworou2006@gmail.com>
2026-05-26 10:36:01 +0000

    refactor: remove boutique creation feature from GNS Admin

--- src/app/features/gns-admin/pages/gns-admin-boutiques/gns-admin-boutiques.component.ts
+++ src/app/features/gns-admin/pages/gns-admin-boutiques/gns-admin-boutiques.component.ts
@@ -1,9 +1,7 @@
 import { Component, signal, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BoutiqueService, BoutiqueResponse } from '../../../../core/services/boutique.service';
 import { ButtonModule } from 'primeng/button';
-import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { TableModule } from 'primeng/table';
@@ -11,15 +9,12 @@ import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-gns-admin-boutiques',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ButtonModule, DialogModule, InputTextModule, FloatLabelModule, TableModule],
+  imports: [CommonModule, ButtonModule, InputTextModule, FloatLabelModule, TableModule],
   templateUrl: './gns-admin-boutiques.component.html',
   styleUrl: './gns-admin-boutiques.component.scss'
 })
 export class GnsAdminBoutiquesComponent implements OnInit {
-  isCreationPanelOpen = signal(false);
-  boutiqueForm: FormGroup;
   boutiques = signal<BoutiqueResponse[]>([]);
-  private fb = inject(FormBuilder);
   private boutiqueService = inject(BoutiqueService);
 
   ngOnInit() {
@@ -33,45 +28,5 @@ export class GnsAdminBoutiquesComponent implements OnInit {
       },
       error: (err) => console.error('Erreur chargement boutiques', err)
     });
-  }
-
-  constructor() {
-    this.boutiqueForm = this.fb.group({
-      nomBoutique: ['', Validators.required],
-      categorieShop: ['', Validators.required],
-      latitude: [null],
-      longitude: [null],
-      initialQuota: [1000000, Validators.required]
-    });
-  }
-
-  openCreationPanel() {
-    this.boutiqueForm.reset({ initialQuota: 1000000 });
-    this.isCreationPanelOpen.set(true);
-  }
-
-  closeCreationPanel() {
-    this.isCreationPanelOpen.set(false);
-  }
-
-  submitForm() {
-    if (this.boutiqueForm.valid) {
-      const payload = {
-        ...this.boutiqueForm.value,
-        statutKYC: 'EN_ATTENTE',
-        cheminCarteEDJ: 'default/path.pdf',
-        // Optional: you can generate or assign merchant tracking ID here later.
-      };
-      this.boutiqueService.create(payload).subscribe({
-        next: (res) => {
-          console.log('Boutique created successfully:', res);
-          this.loadBoutiques();
-          this.closeCreationPanel();
-        },
-        error: (err) => {
-          console.error('Failed to create boutique:', err);
-        }
-      });
-    }
   }
 }
